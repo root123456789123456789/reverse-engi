7ab6f23d448862f9cec25395e421c34e154be6a2
root123456789123456789 <119447572+root123456789123456789@users.noreply.github.com>
2023-12-12 22:04:45 +0100

    Add files via upload

---
+++ Broker/ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using LiteNetLib;
+using Network.Thread;
+
+namespace TeamScreen.Broker.ConsoleApp
+{
+    class Program
+    {
+        public static BrokerThread Manager { get { return Network.Instance.Broker.Instance.Thread; } }
+        
+        static void Main(string[] args)
+        {
+			
+
+			Manager.Events.onPeerConnected += delegate (object sender, EventArgs eventArgs)
+			{
+				NetPeer peer = (NetPeer)sender;
+				Console.WriteLine("Peer connected: " + peer.EndPoint);
+			};
+
+			Manager.Start();
+
+
+            // important!!!
+            Console.TreatControlCAsInput = true;
+
+            while (true)
+            {
+                Console.WriteLine("Use CTRL+C to exit");
+                var input = Console.ReadKey();
+
+                if (input.Key == ConsoleKey.C && input.Modifiers == ConsoleModifiers.Control)
+                {
+                    break;
+                }
+            }
+
+        }
+        
+	}
+}
